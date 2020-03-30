ed1309d1b92e245f3621234a3bed916a2d6ec54b
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-30 12:44:36 -0500

    new Auth0.webauth

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 import auth0 from 'auth0-js';
 // https://auth0.github.io/auth0.js/index.html
 
 export default class Auth {
     constructor(history) {
         this.history = history;
+        this.auth0 = new auth0.WebAuth({
+            domain: process.env.REACT_APP_AUTH0_DOMAIN,
+            clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+            redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+            responseType: "token id_token",
+            scope: "openid profile email"
+        });
     }
 }
