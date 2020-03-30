b75cf6f26625d7ea05613c771277a1e01eef88b4
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-30 11:45:21 -0500

    ra hotkey to make react arrow function

---
+++ client/src/pages/NoMatch.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const NoMatch = () => {
+    return (
+        <div className="text-white">
+            404 Page Not Found
+            <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                🙄
+            </span>
+        </div>
+    )
+};
