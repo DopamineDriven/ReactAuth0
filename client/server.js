8ecda48f210a35d8dc1af4637fae4a9578377883
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-06 02:13:35 -0500

    updates

--- client/server.js
+++ client/server.js
@@ -1,21 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+// require('dotenv').config();
+// const express = require('express');
+// const jwt = require('express-jwt');
+// const jwksRsa = require('jwks-rsa');
 
-// instatiate express
-const app = express();
+// // instatiate express
+// const app = express();
 
-app.get('/public', (req, res) => {
-    res.json({
-        message: "hello from a public API!"
-    })
-    .catch(error)
-})
+// app.get('/public', (req, res) => {
+//     res.json({
+//         message: "hello from a public API!"
+//     })
+//     .catch(error)
+// })
 
-app.get('/private', (req, res) => {
-    res.json({
-        message: "hello from a public API!"
-    })
-    .catch(error)
-})
+// app.get('/private', (req, res) => {
+//     res.json({
+//         message: "hello from a public API!"
+//     })
+//     .catch(error)
+// })
