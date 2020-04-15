acc4b515acfd8da2a575454b17034d34393cacf1
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-14 23:13:13 -0500

    setting up server

--- client/server.js
+++ client/server.js
@@ -1,21 +1,29 @@
-// require('dotenv').config();
-// const express = require('express');
-// const jwt = require('express-jwt');
-// const jwksRsa = require('jwks-rsa');
+require('dotenv').config();
+const express = require('express');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const PORT = process.env.PORT || 3001;
 
-// // instatiate express
-// const app = express();
+// instantiate express
+const app = express();
 
-// app.get('/public', (req, res) => {
-//     res.json({
-//         message: "hello from a public API!"
-//     })
-//     .catch(error)
-// })
+// declare first endpoint (public)
+app.get('/public', (req, res) => {
+    res.json({
+        message: "hello from a public API!"
+    })
+    .catch(error)
+})
 
-// app.get('/private', (req, res) => {
-//     res.json({
-//         message: "hello from a public API!"
-//     })
-//     .catch(error)
-// })
+// declare next endpoint (private)
+app.get('/private', (req, res) => {
+    res.json({
+        message: "hello from a public API!"
+    })
+    .catch(error)
+})
+
+app.listen(PORT, error => {
+    if (error) throw error
+    console.log(`🌎 ==> API Server now listening on PORT http://localhost:${PORT}`)
+});
