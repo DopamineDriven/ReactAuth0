7a108a0308636741655d3ff2d0e1a7811709e190
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-15 03:59:08 -0500

    courses.jsx under construction

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 class Courses extends Component {
+    state = {
+        courses: []
+    }
+
+    componentDidMount() {
+        fetch('/course', {
+            // passing authorization header with Bearer and access token 
+            headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` }
+        })
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('network response was not ok')
+        })
+        .then(response => this.setState({ courses: response.courses }))
+        .catch(error => this.setState({ message: error.message }))
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <ul className="text-white">
+                {this.state.courses.map(course => {
+                    return (
+                    <li key={course.id}>{course.title}</li>
+                    )
+                })}
+            </ul>
         );
     }
 }
