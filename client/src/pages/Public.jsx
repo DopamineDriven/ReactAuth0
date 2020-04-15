b97f8af168cb7f094e0e65a7ec30e3e818bcc647
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-14 23:44:38 -0500

    updated public.jsx

--- client/src/pages/Public.jsx
+++ client/src/pages/Public.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class Public extends Component {
+    state = {
+        message: ""
+    }
+
+    componentDidMount() {
+        fetch('/public').then(response => {
+            if (response.ok) return response.json()
+            throw new Error('network response was not ok')
+        })
+        .then(response => this.setState({ message: response.message }))
+        .catch(error => this.setState({ message: error.message }))
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <p>
+                {this.state.message}
+            </p>
         );
     }
 }
