7ba458238e65fc80064fd87f19b536eb5c357a5d
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-15 06:46:02 -0500

    outlining context usage

--- client/src/utils/AuthContext.jsx
+++ client/src/utils/AuthContext.jsx
@@ -1,3 +1,9 @@
 import React from 'react';
+// instantiate AuthContext
 const AuthContext = new React.createContext();
 export default AuthContext;
+
+// Steps to configure context:
+// (1) Declare context
+// (2) Declare provider (provides data/functions)
+// (3) Declare consumer (consumes data/functions)
