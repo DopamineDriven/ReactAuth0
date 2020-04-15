bf7e5591e12d5e90ee343c4c8ba6ad18abed075a
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-15 05:01:05 -0500

    admin class functioning

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -16,6 +16,17 @@ class Courses extends Component {
         })
         .then(response => this.setState({ courses: response.courses }))
         .catch(error => this.setState({ message: error.message }))
+
+        fetch('/admin', {
+            headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` }
+        })
+        .then(response => {
+            // response.ok ? response.json() : new Error("Network response not okay")
+            if (response.ok) return response.json();
+            throw new Error("Network response not ok")
+        })
+        .then(response => console.log(response))
+        .catch(error => this.setState({ message: error.message }))
     }
 
     render() {
